a76958d47cee66e7aa73e92f9a6a603f362fa492
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-10-02 17:17:17 +0100

    Renaming mesh-forwarder-new to mesh-forwarder

--- services/mesh-forwarder-new/terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-      backend "s3" {
-        bucket = "prm-deductions-terraform-state"
-        key    = "mesh-forwarder/terraform.tfstate"
-        region = var.region
-        encrypt = true
-    }
-}
