86acb78a249f7c6d5ec455ff0e614b6a9cfcb906
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2026-01-23 12:37:53 +0000

    Updated express, babel, jest and nock

--- services/ehr-out-service/test/config.js
+++ services/ehr-out-service/test/config.js
@@ -3,8 +3,8 @@ const AWS_ACCOUNT_NO = '000000000000';
 const LOCALSTACK_URL = 'http://localhost:4566';
 const AWS_REGION = 'eu-west-2';
 export const config = {
-  nhsEnvironment: process.env.NHS_ENVIRONMENT,
-  serviceUrl: process.env.SERVICE_URL,
+  nhsEnvironment: "dev",
+  serviceUrl: "www.example.com",
   localstackEndpointUrl: LOCALSTACK_URL,
   region: AWS_REGION,
   awsAccountNo: AWS_ACCOUNT_NO,
